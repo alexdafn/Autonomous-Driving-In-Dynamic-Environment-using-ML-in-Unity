613a9c208472217cc4230b7a09201470ddddf2ae
alexdafn <alexdaf9812@gmail.com>
2022-08-29 22:26:09 +0300

    Update UIScript.cs

--- UIScript.cs
+++ UIScript.cs
@@ -3,18 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-//using TMPRO
 
 public class UIScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    //1public Text timer;
-    //1public float time;
-    //1float msec;
-    //1float sec;
-    //1float min;
-    //private GamePlay gamePlayObject;
-
     [Header("Component")]
     public Text timer;
     public Text score;
@@ -29,24 +20,12 @@ public class UIScript : MonoBehaviour
     public bool hasLimit;
     public float timerLimit;
 
-    //score from gameplay
     public CarAgentScript scoreDisp;
     public RoadSpawnerManager roadManagerDisp;
-    //public GameObject scoreDisplayObject;
-
-    //PositionCalculation()
-    /*
-    private float totalMeters;
-    private Vector3 previousPosition, currentPosition, differenceVector;
-    private float totalDifference;
-    private Transform blueCar;
-    public float difference; */
 
     void Start()
     {
-        //checking
-        //1StartCoroutine("StopWatch");
-        //blueCar = GameObject.Find("Free_Racing_Car_Blue").transform;
+        //Getting the right reference
         roadManagerDisp = GameObject.FindObjectOfType<RoadSpawnerManager>();
     }
 
@@ -65,44 +44,9 @@ public class UIScript : MonoBehaviour
         
         
         SetTimerText();
-       // SetScoreText();
         SetRoadCounterText();
         SetDifficultyText();
     }
-
-    /*
-    private void FixedUpdate()
-    {
-        previousPosition = blueCar.transform.position;
-    }
-
-    /*
-     private void LateUpdate()
-    {
-        PositionCalculation();
-    }  
-    /*1
-    IEnumerator StopWatch()
-    {
-        while(true)
-        {
-            time += Time.deltaTime;
-            msec = (int)(time-(int)time)*100;
-            sec = (int)(time%60);
-            min = (int)(time/60%60);
-            
-            timer.text = string.Format("{0:00}:{1:00}:{2:00}",min,sec,msec);
-            //timer.text = string.Format("{1:00}",sec);
-
-            yield return null;
-        }
-    }1*/
-
-    /*54
-    private void GetScore()
-    {
-        totalMeters = gamePlayObject.PositionCalculation();
-    }54*/
 
     private void SetTimerText()
     {
@@ -110,7 +54,6 @@ public class UIScript : MonoBehaviour
 
     }
 
-    
     private void SetScoreText()
     {
         float display = scoreDisp.scoreDisplay;
@@ -125,15 +68,5 @@ public class UIScript : MonoBehaviour
     private void SetDifficultyText()
     {
         currentDifficulty.text = roadManagerDisp.difficultySelector.ToString("0.0");
-    }
-
-    //attached into "Reset Level" Button
-    public void ResetScene()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-    }
-
-    ///////EXTRAA///////
-    
-
+    }  
 }
